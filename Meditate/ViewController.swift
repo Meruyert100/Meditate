a27b75a92447500835c2cc09ddc3526534bbce82
Tastandiyeva Meruyert <merutas@mail.ru>
2021-04-17 16:56:57 +0600

    inital commit

---
+++ Meditate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meditate
+//
+//  Created by Meruyert Tastandiyeva on 4/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
